33dba83b287880422d801d3b25a73e94b4e25fc5
SamridhiSona <118939049+SamridhiSona@users.noreply.github.com>
2023-01-24 10:46:20 +0530

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,185 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import  javax.swing.JOptionPane;
+
+class Score extends Rectangle{
+
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+
+    Score(int GAME_WIDTH, int GAME_HEIGHT){
+        Score.GAME_WIDTH = GAME_WIDTH;
+        Score.GAME_HEIGHT = GAME_HEIGHT;
+    }
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Consolas", Font.PLAIN, 60));
+
+        g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
+
+        g.drawString(String.valueOf(player1 / 10) + String.valueOf(player1 % 10), (GAME_WIDTH / 2) - 85, 50);
+        g.drawString(String.valueOf(player2 / 10) + String.valueOf(player2 % 10), (GAME_WIDTH / 2) + 20, 50);
+    }
+
+    public int getPlayer1() {
+        return player1;
+    }
+
+    public int getPlayer2() {
+        return player2;
+    }
+}
+public class GamePanel extends JPanel implements Runnable{
+
+    static final int GAME_WIDTH = 1000;
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+    static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HEIGHT);
+    static final int BALL_DIAMETER = 20;
+    static final int PADDLE_WIDTH = 25;
+    static final int PADDLE_HEIGHT = 100;
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Random random;
+    Paddle paddle1;
+    Paddle paddle2;
+    Ball ball;
+    Score score;
+
+    GamePanel(){
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH,GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void newBall() {
+        random = new Random();
+        ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),random.nextInt(GAME_HEIGHT-BALL_DIAMETER),BALL_DIAMETER,BALL_DIAMETER);
+    }
+    public void newPaddles() {
+        paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
+    }
+    public void paint(Graphics g) {
+        image = createImage(getWidth(),getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
+    }
+    public void draw(Graphics g) {
+        paddle1.draw(g);
+        paddle2.draw(g);
+        ball.draw(g);
+        score.draw(g);
+        Toolkit.getDefaultToolkit().sync(); // I forgot to add this line of code in the video, it helps with the animation
+
+    }
+    public void move() {
+        paddle1.move();
+        paddle2.move();
+        ball.move();
+    }
+    public void checkCollision() {
+
+        //bounce ball off top & bottom window edges
+        if(ball.y <=0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if(ball.y >= GAME_HEIGHT-BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        //bounce ball off paddles
+        if(ball.intersects(paddle1)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; //optional for more difficulty
+            if(ball.yVelocity>0)
+                ball.yVelocity++; //optional for more difficulty
+            else
+                ball.yVelocity--;
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        if(ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; //optional for more difficulty
+            if(ball.yVelocity>0)
+                ball.yVelocity++; //optional for more difficulty
+            else
+                ball.yVelocity--;
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        //stops paddles at window edges
+        if(paddle1.y<=0)
+            paddle1.y=0;
+        if(paddle1.y >= (GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle1.y = GAME_HEIGHT-PADDLE_HEIGHT;
+        if(paddle2.y<=0)
+            paddle2.y=0;
+        if(paddle2.y >= (GAME_HEIGHT-PADDLE_HEIGHT))
+            paddle2.y = GAME_HEIGHT-PADDLE_HEIGHT;
+        //give a player 1 point and creates new paddles & ball
+        if(ball.x <=0) {
+            score.player2++;
+            newPaddles();
+            newBall();
+            System.out.println("Player 2: "+score.player2);
+        }
+        if(ball.x >= GAME_WIDTH-BALL_DIAMETER) {
+            score.player1++;
+            newPaddles();
+            newBall();
+            System.out.println("Player 1: "+score.player1);
+        }
+    }
+    public void run() {
+        //game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks =60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        label:
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now -lastTime)/ns;
+            lastTime = now;
+            if(delta >=1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+                if(score.player1 == 3 || score.player2 ==3){
+                    if (score.player1==3) {
+                        JOptionPane.showMessageDialog(null,"Player 1 wins", "Game Finished",JOptionPane.INFORMATION_MESSAGE);
+                        break label;
+                    }
+                    else
+                        JOptionPane.showMessageDialog(null,"Player 2 wins", "Game Finished",JOptionPane.INFORMATION_MESSAGE);
+                    break label;
+                }
+            }
+        }
+    }
+    public class AL extends KeyAdapter{
+        public void keyPressed(KeyEvent e) {
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
+        }
+        public void keyReleased(KeyEvent e) {
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
+        }
+    }
+}
